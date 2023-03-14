68c7b5a0d3c869e60d16c2540101f3936b692e9d
dave <dave.constantineau@gmail.com>
2023-03-14 14:59:46 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+function pageTransition() {
+    var tl = gsap.timeline();
+
+    // brings up
+    tl.to('ul.transition li', { duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2 });
+    
+    // hides it
+    tl.to('ul.transition li', { duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1 });
+}
+
+function contentAnimation() {
+    var tl = gsap.timeline();
+    tl.from('.left', { duration: 1.5, translateY: 50, opacity: 0 });
+    tl.to('img', { clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" });
+}
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise(done => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+barba.init({
+    sync: true,
+    transitions: [{
+        async leave(data) {
+            const done = this.async();
+
+            pageTransition();
+            await delay(1500);
+            done();
+        },
+        // barba hooks
+        async enter(data) {
+            contentAnimation();
+        },
+
+        async once(data) {
+            contentAnimation();
+        }
+    }]
+})
